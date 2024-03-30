bbec76f20933a1e073f8a1b3e9275b79db1f836b
Nestani123 <nestangagadze96@gmail.com>
2024-03-30 23:34:43 +0400

    almost finished

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
 const API_URL ="https://restcountries.com/v3.1/all";
 
 const countryListContainer = document.querySelector(".countries");
+const searchInput = document.querySelector(".search")[0];
 
 const getCountries = async()=>{
 const response = await fetch(API_URL);
 const data = await response.json();
-for(let i=0; i<data.length; i++){
-    console.log(data[i].name.common);
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `<img class="country_img" src=${data[i].flags.svg} />
-    <h4 class="country_name"> ${data[i].name.common}</h4>
+render(data);
 
-    <div class="country_description">
-    <span><b>Population:</b> ${data[i].population}</span>
-    <span><b>Region:</b> ${data[i].region}</span>
-    <span><b>Capital:</b> ${data[i].capital}</span>
-    </div>
-    `;
-    countryListContainer.append(listItem);
-}
-
+searchInput.addEventListener("keyup", (event) => {
+    const searchValue = event.target.value;
+    const filteredData = data.filter( (country) => 
+    country.name.common.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    render(filteredData);
+    console.log(filteredData);
+    });
 
 };
 
 getCountries();
+const render = (data)=>{
+    countryListContainer.innerHTML = null;
+    for(let i=0; i<data.length; i++){
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<img class="country_img" src=${data[i].flags.svg} />
+        <h4 class="country_name"> ${data[i].name.common}</h4>
+    
+        <div class="country_description">
+        <span><b>Population:</b> ${data[i].population}</span>
+        <span><b>Region:</b> ${data[i].region}</span>
+        <span><b>Capital:</b> ${data[i].capital ? data[i].capital[0]: null}</span>
+        </div>
+        `;
+        countryListContainer.append(listItem);
+    }
+}
